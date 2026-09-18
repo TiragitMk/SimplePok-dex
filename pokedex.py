2f917ffbb5a438a298d6f9a2393cac67ca5c9ec1
Daniel C <danielcabfe@gmail.com>
2026-09-19 00:25:25 +0200

    código comentado y reordenado

--- pokedex.py
+++ pokedex.py
@@ -1,3 +1,6 @@
+"""
+Bucle de pokédex que consulta a la api de pokeapi tras un input, parsea y muestra resultados, salvo que se especifique "exit".
+"""
 import requests as r
 
 poke_set = r.get("https://pokeapi.co/api/v2/pokemon/?limit=1351").json()["results"]
@@ -15,12 +18,15 @@ while True:
     elif pokemon == "exit":
         print("Turning off the pokédex...")
         break
-    poké_request = r.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}")
-    dex_entry = poké_request.json()
+
+    # Hace request, parsea, e inicializa el mensaje.
+    poke_request = r.get(f"https://pokeapi.co/api/v2/pokemon/{pokemon}")
+    dex_entry = poke_request.json()
     pokedex_display = ""
     poke_nombre = dex_entry["name"].capitalize()
     pokedex_display += poke_nombre + ": " f"Number {dex_entry["id"]}.\n"
 
+    # Selecciona y añade los tipos
     tipos = ""
     for tp in dex_entry["types"]:
         if tp["slot"] == 2:
@@ -29,9 +35,11 @@ while True:
 
     mensaje_tipo = poke_nombre + " is a " + tipos + " type pokemon. \n"
 
+    # Selecciona y añade las habilidades.
     habilidades = ""
     for ability in dex_entry["abilities"]:
-        # Los pokémon siempre tienen habilidad oculta, así que siempre acaba en 'and habilidad'.
+        # Los pokémon siempre tienen una habilidad oculta (salvo megas y excepciones), que es slot 3,
+        # así que siempre acaba en 'and habilidad', y no en ', habilidad'. Si no tiene oculta, no es necesario esto.
         if ability["slot"] == 2:
             habilidades += ", "
         elif ability["slot"] == 3:
@@ -42,5 +50,5 @@ while True:
         mensaje_habilidad = "Its ability is " + habilidades + "."
     elif len(dex_entry["abilities"]) > 1:
         mensaje_habilidad = "Its abilities are " + habilidades + "."
-
+        
     print(pokedex_display + mensaje_tipo + mensaje_habilidad)
